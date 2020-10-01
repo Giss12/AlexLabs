fdbd9069ed7eb9eb07990b1d0533be00143d4bcc
Giss12 <36027903+Giss12@users.noreply.github.com>
2020-10-01 21:09:03 +0300

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            List<int> numbers = new List<int>(10);
+            for (int i = 0; i < 10; i++)
+            {
+                parts.Add(rand.Next(0, 10));
+            }
+            numbers.RemoveAt(3);
+            numbers.RemoveAt(5);
+            numbers.Add(12);
+            numbers.ForEach(Console.WriteLine);
+        }
+    }
+}
